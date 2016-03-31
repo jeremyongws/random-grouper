864c0cf78864dffcab32e0a36f17b845bd7d2d12
Jeremy Ong <jeremy.ongws@gmail.com>
2016-03-31 16:21:55 +0800

    habis

--- StudentRandomiser/ViewController.m
+++ StudentRandomiser/ViewController.m
@@ -9,19 +9,52 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *studentLabels;
+@property NSArray *students;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.students =  @[@"Jeremy", @"Ming Xiang", @"Abdo", @"Faris", @"Daniel", @"Wilson", @"Michael"];
+	for (UILabel *student in self.studentLabels){
+		student.text = @"";
+	}
+	
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onGeneratePressed:(id)sender {
+	NSMutableArray *randomizedArray = [[NSMutableArray alloc] initWithArray:self.students];
+
+	NSUInteger count = [self.students count];
+	if (count < 1) return;
+	for (NSUInteger i = 0; i < count - 1; ++i) {
+		NSInteger remainingCount = count - i;
+		NSInteger exchangeIndex = i + arc4random_uniform((u_int32_t)remainingCount);
+		[randomizedArray exchangeObjectAtIndex:i withObjectAtIndex:exchangeIndex];
+	}
+	
+	for (UILabel *student in self.studentLabels) {
+		if ([randomizedArray count] != 0){
+			if ([randomizedArray count] == 1){
+					student.text = [[randomizedArray objectAtIndex:0] stringByAppendingFormat:@" joins group %u", arc4random_uniform(3) + 1];
+			} else {
+					student.text = [[randomizedArray objectAtIndex:0] stringByAppendingString:@" "];
+			}
+			[randomizedArray removeObjectAtIndex:0];
+		}
+		else {
+			break;
+		}
+	}
+
+	
 }
 
 @end
